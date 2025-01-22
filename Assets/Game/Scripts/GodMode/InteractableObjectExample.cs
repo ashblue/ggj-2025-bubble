a238cb3523d6e5b57d4604298deba3794e9b440e
Ash Blue <ash@clevercrowgames.com>
2025-01-21 16:46:19 -0800

    PIP Monitor Sample With Clicks To Screen Interactions (#5)
    * Creation of screen with PIP and volume profiles
    
    * Clicking PIP texture now renders clicks in the PIP source camera
    
    * leftover file change

---
+++ Assets/Game/Scripts/GodMode/InteractableObjectExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameJammers.GGJ2025.GodMode {
+    public class InteractableObjectExample : InteractableObjectBase {
+        protected override void OnPipInteract () {
+            Debug.Log("Interacted with " + name);
+        }
+        
+        protected override void OnPipHover () {
+            Debug.Log("Hovered over " + name);
+        }
+    }
+}
