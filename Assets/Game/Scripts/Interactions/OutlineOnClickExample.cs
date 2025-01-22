38b4ab3040da35ff57430ed92ddf07a9d3df6285
Grahame Watt <watt.grah@gmail.com>
2025-01-22 10:41:33 -0800

    Outline on click

---
+++ Assets/Game/Scripts/Interactions/OutlineOnClickExample.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using ChrisNolet.QuickOutline;
+
+#nullable enable
+
+namespace GameJammers.GGJ2025.GodMode
+{
+    public class OutlineOnClickExample : InteractableObjectBase
+    {
+        private Outline _outline = default!;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out Outline res))
+            {
+                _outline = res;
+                _outline.enabled = false;
+            }
+            else
+            {
+                Debug.LogWarning("Can't find Outline Component. Disabling");
+                gameObject.SetActive(false);
+            }
+        }
+
+        protected override void OnPipInteract()
+        {
+            _outline.enabled = !_outline.enabled;
+        }
+    }
+}
+
