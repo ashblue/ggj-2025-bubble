e57e0daef8e6c117d9cbfacd4152b1468a5658ef
Ash Blue <ash@clevercrowgames.com>
2025-01-24 13:32:35 -0800

    Refactored explodable code so it can be overriden with implementation hooks

--- Assets/Game/Scripts/Explodables/Editor/ExplodableEditor.cs
+++ Assets/Game/Scripts/Explodables/Editor/ExplodableEditor.cs
@@ -2,14 +2,14 @@
 using UnityEngine;
 
 namespace GameJammers.GGJ2025.Explodables {
-    [CustomEditor(typeof(Explodable))]
+    [CustomEditor(typeof(ExplodableExample))]
     public class ExplodableEditor : Editor {
         public override void OnInspectorGUI() {
             base.OnInspectorGUI();
 
             // Add a button to the inspector when the game is playing
             if (Application.isPlaying) {
-                var explodable = (Explodable)target;
+                var explodable = (ExplodableExample)target;
 
                 if (GUILayout.Button("Explode")) {
                     explodable.Explode();
--- Assets/Game/Scripts/Explodables/TriggerExplodableTracker.cs
+++ Assets/Game/Scripts/Explodables/TriggerExplodableTracker.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 namespace GameJammers.GGJ2025.Explodables {
     public class TriggerExplodableTracker : MonoBehaviour {
-        readonly List<Explodable> _trackedObjects = new();
+        readonly List<ExplodableBase> _trackedObjects = new();
 
-        public IReadOnlyList<Explodable> TrackedObjects => _trackedObjects;
+        public IReadOnlyList<ExplodableBase> TrackedObjects => _trackedObjects;
 
         void OnTriggerEnter (Collider other) {
-            var explodable = other.gameObject.GetComponent<Explodable>();
+            var explodable = other.gameObject.GetComponent<ExplodableBase>();
             if (explodable) {
                 _trackedObjects.Add(explodable);
             }
         }
 
         void OnTriggerExit (Collider other) {
-            var explodable = other.gameObject.GetComponent<Explodable>();
+            var explodable = other.gameObject.GetComponent<ExplodableBase>();
             if (explodable) {
                 _trackedObjects.Remove(explodable);
             }
