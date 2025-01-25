2258941ae57e01b861a660d8f234a61c105b1c93
MaximGameDev <56001077+MaximGameDev@users.noreply.github.com>
2025-01-25 12:37:24 -0800

    Button Outline
    The Main Room button should now have an orange outline around it when hovered over by the mouse.

--- Assets/Game/Scripts/Game/ExplosionButton.cs
+++ Assets/Game/Scripts/Game/ExplosionButton.cs
@@ -1,14 +1,35 @@
-﻿using UnityEngine;
+using ChrisNolet.QuickOutline;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace GameJammers.GGJ2025.Bootstraps {
     public class ExplosionButton : MonoBehaviour {
         GameController _game;
+        private Outline buttonoutline;
 
         void Start () {
             _game = GameController.Instance;
+            TryGetComponent(out buttonoutline);
+            if(buttonoutline != null) {
+                buttonoutline.enabled = false;
+            }
+        }
+
+        void OnMouseEnter () {
+            if (buttonoutline == null) { return; }
+            buttonoutline.enabled = true;
+        }
+
+        void OnMouseExit () {
+            if (buttonoutline == null) { return; }
+            buttonoutline.enabled = false;
         }
 
         void OnMouseDown () {
+            if (buttonoutline != null) {
+                buttonoutline.OutlineColor = Color.magenta;
+            }
+
             if (!_game) {
                 Debug.LogWarning("Explosions cannot be tested without a GameController instance. You probably need to additively load the Main scene to globally load it.");
                 return;
