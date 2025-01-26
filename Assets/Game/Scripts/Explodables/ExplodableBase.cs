d8cd1f0ac527fc9ffebb06bfda8357c0230a3052
NAPTlME <napratt2@yahoo.com>
2025-01-25 20:31:38 -0600

    Cleaning up

--- Assets/Game/Scripts/Explodables/ExplodableBase.cs
+++ Assets/Game/Scripts/Explodables/ExplodableBase.cs
@@ -13,12 +13,6 @@ namespace GameJammers.GGJ2025.Explodables {
         [SerializeField]
         bool _isObjective;
 
-        /* Removing this for now. The base poppable can be extended to add colliders or use physics calls.
-         The base poppable will use a sphere cast
-        [Tooltip("You can use any explosion shape you want. The trigger area just needs to have a TriggerExplodableTracker on it.")]
-        [SerializeField]
-        TriggerExplodableTracker _explosionTrigger;*/
-
         public bool AutoExplode => _autoExplode;
         public bool IsObjective => _isObjective;
         public bool IsPrimed { get; private set; } // now only used for scoring
@@ -40,35 +34,6 @@ namespace GameJammers.GGJ2025.Explodables {
             PlayAnimation(); // may hide this for now?
 
         }
-
-        /*IEnumerator ExplosionLoop () {
-            // this functionality is moving over to PopManager
-            // Prevent recursive detonations by setting the primed flag
-            IsPrimed = true;
-
-            // Track this globally so we can wait for all explosions to resolve
-            _collection.AddExploding(this);
-
-            PlayAnimation();
-            // timing is handled by PopManager and poppables now
-            //while (!GetIsAnimationComplete()) {
-            //    yield return null;
-            //}
-
-            // there is a slight difference in the implication of IsPrimed and Poppable.CanPop
-            // Can Pop will handle this and IsPrimed can be used for scoring
-            // Find all explodables inside the explosion area
-            //foreach (var target in _explosionTrigger.TrackedObjects) {
-            //    if (!target.IsPrimed) {
-            //        target.Explode();
-            //    }
-            //}
-
-            // Inform the game state that this explosion has resolved
-            //_collection.RemoveExploding(this);
-
-            //ExplosionComplete();
-        }*/
 
         void PlayAnimation() {
             OnPlayAnimation();
