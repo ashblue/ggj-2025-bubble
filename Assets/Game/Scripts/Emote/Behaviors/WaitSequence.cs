de614e4745a41350fca1c082242d5398270024ef
NAPTlME <napratt2@yahoo.com>
2025-01-26 19:48:56 -0600

    Could not remove the debug log in favor of an empty or trivial callback. Resulted in many failed/killed tweens

--- Assets/Game/Scripts/Emote/Behaviors/WaitSequence.cs
+++ Assets/Game/Scripts/Emote/Behaviors/WaitSequence.cs
@@ -11,7 +11,7 @@ namespace GameJammers.GGJ2025.Emote.Behaviors {
         public override Sequence BuildSequence () {
             emoteSequence = DOTween.Sequence ();
             emoteSequence.AppendInterval(Duration);
-            emoteSequence.AppendCallback(() => Debug.Log("Wait sequence finished")); // have to do something or no sequence
+            emoteSequence.AppendCallback(() => Debug.Log("Wait sequence finished")); // have to do something or no sequence // could not replace this with an empty or with a call that toggled a bool
             return emoteSequence;
         }
     }
